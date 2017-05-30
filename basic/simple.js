d89c4be0c9eaee25be24636ce7a5026ea2d91cdd
paramothers <param.others@gmail.com>
2017-05-30 17:06:37 +0530

    typescript access modifier

--- basic/simple.js
+++ basic/simple.js
@@ -2,4 +2,11 @@ class Person {
 }
 let person;
 person.names = `Paramasivam k`;
+class Wife extends Person {
+    constructor(jobTitle) {
+        super();
+        this.jobTitle = jobTitle;
+        this.relationship = [];
+    }
+}
 //# sourceMappingURL=simple.js.map
--- basic/simple.ts
+++ basic/simple.ts
@@ -8,3 +8,13 @@ class Person{
 let person : Person;
 
 person.names = `Paramasivam k`;
+
+class Wife extends Person{
+
+    public sex : string;
+    public relationship = [];
+    constructor(public jobTitle: string){
+        super();
+        this.relationship.push(jobTitle);
+    }
+}
