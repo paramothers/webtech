ee58c1011159c220fbe27ac41395730f2cd1bbdc
paramothers <param.others@gmail.com>
2017-06-02 00:15:43 +0530

    cordova plugins used

--- cordovadummy/CordovaProject/www/js/index.js
+++ cordovadummy/CordovaProject/www/js/index.js
@@ -18,7 +18,7 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -26,12 +26,14 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
+
+        document.getElementById("cameraTakePicture").addEventListener("click", cameraTakePicture);
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -40,7 +42,192 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+
+        document.getElementById("setLocalStorage").addEventListener("click", setLocalStorage);
+        document.getElementById("showLocalStorage").addEventListener("click", showLocalStorage);
+        document.getElementById("removeProjectFromLocalStorage").addEventListener("click", removeProjectFromLocalStorage);
+        document.getElementById("getLocalStorageByKey").addEventListener("click", getLocalStorageByKey);
+
+        document.getElementById("createContact").addEventListener("click", createContact);
+        document.getElementById("findContact").addEventListener("click", findContact);
+        document.getElementById("deleteContact").addEventListener("click", deleteContact);
+
+        document.getElementById("cordovaDevice").addEventListener("click", cordovaDevice);
+
+
+        document.getElementById("getAcceleration").addEventListener("click", getAcceleration);
+        document.getElementById("watchAcceleration").addEventListener("click", watchAcceleration);
+        var localStorage = window.localStorage;
+
+
     }
 };
+function cameraGetPicture() {
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    });
 
+    function onSuccess(imageURL) {
+        var image = document.getElementById('myImage');
+        image.src = imageURL;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+
+}
+
+function cameraTakePicture() {
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
+
+    function onSuccess(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
+
+function setLocalStorage() {
+    localStorage.setItem("Name", "John");
+    localStorage.setItem("Job", "Developer");
+    localStorage.setItem("Project", "Cordova Project");
+}
+
+function showLocalStorage() {
+    console.log(localStorage.getItem("Name"));
+    console.log(localStorage.getItem("Job"));
+    console.log(localStorage.getItem("Project"));
+}
+
+function removeProjectFromLocalStorage() {
+    localStorage.removeItem("Project");
+}
+
+function getLocalStorageByKey() {
+    console.log(localStorage.key(0));
+}
+
+
+function createContact() {
+    var myContact = navigator.contacts.create({ "displayName": "Test User" });
+    myContact.save(contactSuccess, contactError);
+
+    function contactSuccess() {
+        alert("Contact is saved!")
+    }
+
+    function contactError(message) {
+        alert('Failed because: ' + message);
+    }
+
+}
+
+function findContacts() {
+    var options = new ContactFindOptions();
+    options.filter = "";
+    options.multiple = true;
+
+    fields = ["displayName"];
+    navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+
+    function contactfindSuccess(contacts) {
+        for (var i = 0; i < contacts.length; i++) {
+            alert("Display Name = " + contacts[i].displayName);
+        }
+    }
+
+    function contactfindError(message) {
+        alert('Failed because: ' + message);
+    }
+
+}
+
+function deleteContact() {
+
+    var options = new ContactFindOptions();
+    options.filter = "Test User";
+    options.multiple = false;
+    fields = ["displayName"];
+
+    navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+
+    function contactfindSuccess(contacts) {
+
+        var contact = contacts[0];
+        contact.remove(contactRemoveSuccess, contactRemoveError);
+
+        function contactRemoveSuccess(contact) {
+            alert("Contact Deleted");
+        }
+
+        function contactRemoveError(message) {
+            alert('Failed because: ' + message);
+        }
+    }
+
+    function contactfindError(message) {
+        alert('Failed because: ' + message);
+    }
+
+}
+
+function cordovaDevice() {
+    alert("Cordova version: " + device.cordova + "\n" +
+        "Device model: " + device.model + "\n" +
+        "Device platform: " + device.platform + "\n" +
+        "Device UUID: " + device.uuid + "\n" +
+        "Device version: " + device.version);
+}
+
+
+function getAcceleration() {
+    navigator.accelerometer.getCurrentAcceleration(accelerometerSuccess, accelerometerError);
+
+    function accelerometerSuccess(acceleration) {
+        alert('Acceleration X: ' + acceleration.x + '\n' +
+            'Acceleration Y: ' + acceleration.y + '\n' +
+            'Acceleration Z: ' + acceleration.z + '\n' +
+            'Timestamp: ' + acceleration.timestamp + '\n');
+    };
+
+    function accelerometerError() {
+        alert('onError!');
+    };
+
+}
+
+function watchAcceleration() {
+
+    var accelerometerOptions = {
+        frequency: 3000
+    }
+
+    var watchID = navigator.accelerometer.watchAcceleration(accelerometerSuccess, accelerometerError, accelerometerOptions);
+
+    function accelerometerSuccess(acceleration) {
+        alert('Acceleration X: ' + acceleration.x + '\n' +
+            'Acceleration Y: ' + acceleration.y + '\n' +
+            'Acceleration Z: ' + acceleration.z + '\n' +
+            'Timestamp: ' + acceleration.timestamp + '\n');
+
+        setTimeout(function () {
+            navigator.accelerometer.clearWatch(watchID);
+        }, 10000);
+
+    };
+
+    function accelerometerError() {
+        alert('onError!');
+    };
+
+}
 app.initialize();
