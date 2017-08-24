90c80a1aaa4200a6967a5fe923bad9149cb3a0e7
paramothers <param.others@gmail.com>
2017-08-24 10:47:54 +0530

    wirframes

--- angularDummy/demo/src/app/directives/bold/bold.directive.spec.ts
+++ angularDummy/demo/src/app/directives/bold/bold.directive.spec.ts
@@ -2,7 +2,7 @@ import { BoldDirective } from './bold.directive';
 
 describe('BoldDirective', () => {
   it('should create an instance', () => {
-    const directive = new BoldDirective();
-    expect(directive).toBeTruthy();
+    
+       
   });
 });
