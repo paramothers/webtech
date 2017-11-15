9c60e074bf6aeb4369871ebc5aa432e90e107088
paramothers <param.others@gmail.com>
2017-11-16 00:08:04 +0530

    In Memory web api fetch completed

--- angularDummy/tour-of-heros/src/app/services/hero.service.ts
+++ angularDummy/tour-of-heros/src/app/services/hero.service.ts
@@ -4,22 +4,55 @@ import { Observable } from "rxjs/Rx";
 import { of } from "rxjs/observable/of";
 import { MessageService } from "./message.service";
 import { HttpClient } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable()
 export class HeroService {
-  private heroesUrl: string = "api/heroes";
+  private heroesUrl = "api/heroes";
   constructor(
     private http: HttpClient,
     private messageService: MessageService
   ) {}
 
   getHeros(): Observable<Hero[]> {
-    this.messageService.add("HeroService : fetch completed");
-    return of(HEROS);
+    this.log("HeroService : fetch completed");
+
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log(`fetched heroes`)),
+        catchError(this.handleError("getHeros", []))
+      );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   getHero(id: number): Observable<Hero> {
-    return of(HEROS.find(hero => hero.id === id));
+    let urlTemp = `${this.heroesUrl}/${id}`;
+    return this.http
+      .get<Hero>(urlTemp)
+      .pipe(
+        tap(_ => this.log(`the hero fetched ${id}`)),
+        catchError(this.handleError<Hero>(`getHero${id}`))
+      );
+
   }
   private log(message: string) {
     this.messageService.add("HeroService : " + message);
