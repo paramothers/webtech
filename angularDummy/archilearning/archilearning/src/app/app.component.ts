5e5b0116da0e9c8f000f9d558c8d9c6fa8ba7c86
paramothers <param.others@gmail.com>
2017-11-26 23:16:40 +0530

    architectural learning

--- angularDummy/archilearning/archilearning/src/app/app.component.ts
+++ angularDummy/archilearning/archilearning/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Hero } from "app/hero";
 
 @Component({
   selector: "app-root",
@@ -7,20 +8,30 @@ import { Component } from "@angular/core";
  <h1>{{title}}</h1><br>
 
   <ul>
-    <li *ngFor="let hero of heros">{{hero}}</li>
+    <li *ngFor="let hero of heroes">{{hero.id}} - {{hero.name}}</li>
   </ul>
-  <h3>My Favourate hero {{myHero}}</h3>
+  <h3>My Favourate hero {{myHero.name}}</h3>
+  <p *ngIf="heroes.length > 2">There are more Heros !</p>
+
+  <img style="height:30px" src="{{imageUrl}}">
+<p>The sum of 1 + 2 is : {{1+2+getVal()}}</p>
   `,
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   title: string;
-  myHero: string;
-  heros: string[] = ["ramarajan", "vijaykanth", "sathiyaraj"];
-
+  myHero: Hero;
+  imageUrl: string = "../favicon.ico";
+  //heros: string[] = ["ramarajan", "vijaykanth", "sathiyaraj"];
+  heroes = [new Hero(1, "param"), new Hero(2, "pavalan"), new Hero(3, "padhu")];
   constructor() {
     this.title = "Angular Architecture learning !";
-    this.myHero = "Pavalan";
-    this.myHero = this.heros[0];
+    // this.myHero = "Pavalan";
+    this.myHero = this.heroes[0];
+  }
+
+  getVal() : number{
+
+     return 23;
   }
 }
