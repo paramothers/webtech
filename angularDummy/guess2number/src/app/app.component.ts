44a43f3ded0503b472548b36d7d9b22d4efea9ed
paramothers <param.others@gmail.com>
2018-11-13 21:59:07 +0530

    guess number

--- angularDummy/guess2number/src/app/app.component.ts
+++ angularDummy/guess2number/src/app/app.component.ts
@@ -4,28 +4,60 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
+    <div style="text-align:center" class='container'>
       <h1>
-        Welcome to {{title}}!
+       Guess Number 2 !
       </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+      <div class="card bg-light mb-3">
+         <div class="card-body">
+            <p class="card-text"> Guess the number between 1 and 1000</p>
+         </div>
+      </div>
+      <div>
+          <label>Your Guess: </label>
+          <input type="number" [value]="guess" (input)="guess = $event.target.value" />
+          <button (click)="verifyGuess()" class="btn btn-primary btn-sm">Verify</button>
+          <button (click)="initializeGame()" class="btn btn-warning btn-sm">Restart</button>
+      </div>
+
+      <div>
+         <p *ngIf="deviation < 0" class="alert alert-warning">Your Guess is higher</p>
+         <p *ngIf="deviation > 0" class="alert alert-warning">Your Guess is lower</p>
+         <p *ngIf="deviation === 0" class="alert alert-success">Yes. That's right !</p>
+      </div>
+
+      <p class="text-info"> No of guess:
+        <span class="badge">{{noOfTries}}</span>
+      </p>
+
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://github.com/angular/angular-cli/wiki">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+
+
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  guess: number;
+  deviation: number;
+  noOfTries: number;
+  original: number;
+
+  constructor() {
+
+    this.initializeGame();
+  }
+  verifyGuess(): void {
+
+    this.deviation = this.original - this.guess;
+    this.noOfTries = this.noOfTries + 1;
+  }
+
+  initializeGame(): void {
+
+    this.noOfTries = 0;
+    this.deviation = null;
+    this.guess = null;
+    this.original = Math.floor((Math.random() * 1000) + 1);
+  }
 }
