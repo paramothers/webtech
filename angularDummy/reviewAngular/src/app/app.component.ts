d7ebfcda6741e457966ee61f59316c72a9419f5e
paramothers <param.others@gmail.com>
2019-09-18 22:46:54 +0530

    angular 3rd round

--- angularDummy/reviewAngular/src/app/app.component.ts
+++ angularDummy/reviewAngular/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reviewAngular';
+
+  deviation: number;
+  noOfTries: number;
+  original: number;
+  guess: number;
+
+  constructor() {
+    this.initializeGame();
+  }
+
+  initializeGame() {
+
+    this.noOfTries = 0;
+    this.noOfTries = null;
+    this.guess = null;
+    this.original = Math.floor((Math.random() * 1000) + 1);
+  }
+
+  verifyGuess() {
+    this.deviation = this.original - this.guess;
+    this.noOfTries = this.noOfTries + 1;
+  }
 }
