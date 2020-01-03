6e16e6be63c9305f067e8021e5684d45706baa18
paramothers <param.others@gmail.com>
2020-01-03 12:47:31 +0530

    GraphQL study

---
+++ reactNative/ToDo/app/TodoList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View} from 'react-native';
+import Todo from './Todo';
+
+const TodoList = ({todos, deleletTodo, toggleComplete, type}) => {
+  const getVisibleTodos = (todos, type) => {
+    switch (type) {
+      case 'All':
+        return todos;
+      case 'Complete':
+        return todos.filter(t => t.complete);
+      case 'Active':
+        return todos.filter(t => !t.complete);
+    }
+  };
+  todos = getVisibleTodos(todos, type);
+  todos = todos.map((todo, i) => {
+    return (
+      <Todo
+        key={i}
+        todo={todo}
+        deleletTodo={deleletTodo}
+        toggleComplete={toggleComplete}
+      />
+    );
+  });
+  return <View>{todos}</View>;
+};
+
+export default TodoList;
